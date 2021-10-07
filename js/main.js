550789ce343214b8909a545ae99af37a67b1ddd5
hamakai11 <91622644+hamakai11@users.noreply.github.com>
2021-10-08 01:55:46 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,57 @@
+'use strict';
+
+{
+  class Panel {
+    constructor() {
+      const section = document.createElement('section');
+      section.classList.add('panel');
+
+      this.img = document.createElement('img');
+      this.img.src = this.getRandomImage();
+
+      this.timeoutId = undefined;
+
+      this.stop = document.createElement('div');
+      this.stop.textContent = 'STOP';
+      this.stop.classList.add('stop');
+      this.stop.addEventListener('click', () => {
+        clearTimeout(this.timeoutId);
+      });
+
+      section.appendChild(this.img);
+      section.appendChild(this.stop);
+
+      const main = document.querySelector('main');
+      main.appendChild(section);
+    }
+
+    getRandomImage() {
+      const images = [
+        'IMG_6678.JPG',
+        'IMG_E4007.JPG',
+        'IMG_E0969.JPG',
+      ];
+      return images[Math.floor(Math.random() * images.length)];
+    }
+
+    spin() {
+      this.img.src = this.getRandomImage();
+      this.timeoutId = setTimeout(() => {
+        this.spin();
+      }, 50);
+    }
+  }
+
+  const panels = [
+    new Panel(),
+    new Panel(),
+    new Panel(),
+  ];
+
+  const spin = document.getElementById('spin');
+  spin.addEventListener('click', () => {
+    panels.forEach(panel => {
+      panel.spin();
+    });
+  });
+}
